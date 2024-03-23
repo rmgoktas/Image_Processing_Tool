a6a5005c683bb8fc60a2630ae8cf581536897433
Metin Agan <130462728+Metinagan@users.noreply.github.com>
2024-03-24 01:50:34 +0300

    added zoom in and out function

---
+++ zoom.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+
+def resize_image(img, scale):
+    # Eski boyutları al
+    height, width = img.shape[:2]
+    # Yeni boyutu al
+    new_width=width*scale
+    new_height=height*scale
+    # Yeni boş bir görüntü oluştur
+    resized_img = np.zeros((new_height, new_width, 3), dtype=np.uint8)
+    
+    # Boyut oranlarını hesapla
+    width_ratio = width / new_width
+    height_ratio = height / new_height
+    
+    # Yeniden boyutlandırma işlemi
+    for y in range(new_height):
+        for x in range(new_width):
+            resized_x = int(x * width_ratio)
+            resized_y = int(y * height_ratio)
+            resized_img[y, x] = img[resized_y, resized_x]
+    
+    return resized_img 
