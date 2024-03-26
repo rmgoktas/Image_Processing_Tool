3f98b79e2c3e57909c561ecbb753ebed1e33a7e8
Metinagan <metin.agan01@gmail.com>
2024-03-26 22:51:24 +0300

    added box_blurring

---
+++ blurring.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+def box_blurring(image, kernelsize):
+    # Resmin boyutlarını al
+    height, width = image.shape[0:2]
+
+    kernel = create_kernel(kernelsize)
+
+    # Yeni resim
+    blurred_image = np.zeros_like(image, dtype=np.float32)
+
+    for y in range(kernelsize // 2, height - kernelsize // 2):
+        for x in range(kernelsize // 2, width - kernelsize // 2):
+            neighborhood = image[y - kernelsize // 2 : y + kernelsize // 2 + 1,
+                                 x - kernelsize // 2 : x + kernelsize // 2 + 1]
+            # Axis(0,1) önce ilk satır için daha sonra diğer satırlar için sırayla işlemi gerçekleştirir
+            blurred_pixel = np.sum(neighborhood * kernel, axis=(0, 1))
+            blurred_image[y, x] = blurred_pixel
+
+    # Blurlanmış resmi uint8 formata dönüştürüyoruzz
+    blurred_image = np.clip(blurred_image, 0, 255).astype(np.uint8)
+    return blurred_image
+
+# Değişken boyutlu kernel oluşturmak için kullanıyoruz 
+def create_kernel(size):
+    kernel_value = 1 / (size * size)
+    kernel = np.full((size, size), kernel_value)
+    return kernel
--- trial_file.py
+++ trial_file.py
@@ -1,16 +1,14 @@
-from adaptive_threshold import threshold
+from blurring import box_blurring
 import cv2
 
 img1=cv2.imread("cameraman.png")
 gray=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 
-thres=threshold(gray,255,2,3)
-adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3,0)
-cv2.imshow("thres",thres)
-cv2.imshow("adaptive",adaptive_threshold)
+
+blurimg=box_blurring(gray,3)
 
 
-
+cv2.imshow("blur",blurimg)
 
 
 
